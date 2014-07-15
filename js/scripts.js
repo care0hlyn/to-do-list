8bfc3d072065a7a5d748550776d1cb8546c6c0f7
Justin Speers & Carolyn Huynh <student@epicodus.com>
2014-07-15 13:43:27 -0700

    add ability to remove tasks when complete by clicking

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,38 @@
 
 $(document).ready(function() {
-  $("#new-contact-form").submit(function(event) {
+  $("form#new-contact-form").submit(function(event) {
     event.preventDefault();
 
+    // var msg = "Task: + " + $("#task").val();
+
+    // if ($("#urgent-checkbox").is(":checked")) {
+    //   msg = msg + " is urgent";
+    // } else {
+    //   msg = msg + " is not urgent";
+    // }
+
+    // alert(msg);
+
+    var taskDescription = $("#task").val();
+    var taskUrgent = $("#urgent-checkbox").is(":checked");
+
+    var thisTask = {description: taskDescription,
+                    isUrgent: taskUrgent};
+
+
+    if (thisTask.isUrgent) {
+      $("#task-list").append("<li class='urgentClass'>" + thisTask.description +
+                              "</li>")
+    } else {
+      $("#task-list").append("<li>" + thisTask.description + "</li>");
+    }
+
+    $("#task-list li").last().click(function() {
+      $(this).remove();
+    });
+
+    $("#task").val("");
+    $("#urgent-checkbox").prop('checked', false);
 
   });
 
